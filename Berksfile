1e3665342ff537b88efd4edb110b8aaf417ee1d3
Morgan Sadr-Hashemi <morgan.sadr@bt.com>
2015-08-20 10:08:06 +0100

    Berksfile depends on chef ingredient instead

--- Berksfile
+++ Berksfile
@@ -2,5 +2,4 @@ source "https://supermarket.chef.io"
 
 metadata
 
-cookbook 'chef-server-ingredient', '~> 0.4.0'
-#cookbook 'chef-server-ingredient', git: 'https://github.com/chef-cookbooks/chef-server-ingredient.git'
+cookbook 'chef-ingredient'
--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license          'all_rights'
 description      'Installs and Configures Supermarket from the Omnibus packages on packagecloud.io'
 source_url       'https://github.com/irvingpop/supermarket-omnibus-cookbook'
 issues_url       'https://github.com/irvingpop/supermarket-omnibus-cookbook/issues'
-version          '0.4.0'
+version          '0.4.1'
 
 
-depends 'chef-server-ingredient'
+depends 'chef-ingredient'
 depends 'hostsfile'
